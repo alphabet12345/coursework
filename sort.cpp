52a1218f57ef34697a63329d4e4c7e64c13832b5
Svetlana <ssvetlana311223@gmail.com>
2017-05-23 01:03:50 +0700

    Added function selection

--- sort.cpp
+++ sort.cpp
@@ -6,3 +6,32 @@
 #include <cstdlib>
 
 using namespace std;
+
+int selection();
+char text[100];
+
+int selection ()
+ {
+   curNumb=0;
+   int k, q ,c;
+   int d;
+   char slovo[13];
+   k=strlen(text);
+   int i=0, j=0;
+   while (i<k) 
+	{
+      j=0;
+      while((text[i]!=' ')&&(text[i]!='\0')) 
+		{
+		  slovo[j]=text[i];
+          i++;
+          j++;
+        }
+      i++;
+      slovo[j]='\0';
+	  puts(slovo);
+	 
+	  d=strlen(slovo); 
+	}
+ }
+
