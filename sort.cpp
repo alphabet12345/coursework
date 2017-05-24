2137b573cec9f87a81ecd44afaa46ff11b06bec9
LeraP <torens1998@gmail.com>
2017-05-24 16:47:45 +0700

    Fixed code#2

--- sort.cpp
+++ sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <string.h>
-#include <locale.h>
 #include <cstring>
+#include <string>
+#include <locale.h>
 
 using namespace std;
 
@@ -24,39 +24,41 @@ int main ()
    fclose(f);
    return 0;
  }
-
-int selection ()
+ 
+int selection () 
  {
    string words[20] = {""};
-   int wordsAmount = -1;
+   int wordsAmount = -1; 	
    curNumb=0;
-   int k, q , c, o, p, l;
-   int d;
+   int k, q ,c, o, p, l;
    int y[p];
+   int d;
    char slovo[13];
    char u[o];
    char temp;
    k=strlen(text);
    int i=0, j=0;
    while (i<k) 
-    {
+	{
       j=0;
       wordsAmount++;
       while((text[i]!=' ')&&(text[i]!='\0')) 
-	{
-	  slovo[j]=text[i];
-          words[wordsAmount] += text[i];
+		{
+		  slovo[j]=text[i];
+		  words[wordsAmount] += text[i];
           i++;
           j++;
         }
       i++;
-    } 
-   selectSort(words,wordsAmount);
-   for(int _i=0; _i<wordsAmount; _i++) 
-    {
-      cout << words[_i] << endl;
     }
+	selectSort(words,wordsAmount);
+	for(int _i=0; _i<wordsAmount; _i++) 
+     {
+		cout << words[_i] << endl;
+	 }
+			
  }
+     
 
 bool isLess(string a, string b) 
  {
@@ -64,33 +66,33 @@ bool isLess(string a, string b)
    int bLen = b.length();
    int len = aLen;
    if (bLen < aLen) len = bLen;
-    for(int i=0; i<len; i++) 
-      {		
-	if (a[i] < b[i]) 
-          {			
-    	    return 1;	
-	  }
-        if (a[i] > b[i]) 
-          {			
-	    return 0;	
-          }
-       }
+   for(int i=0; i<len; i++) 
+    {		
+	  if (a[i] < b[i]) 
+       {			
+		 return 1;	
+	   }
+	  if (a[i] > b[i])
+       {			
+		 return 0;	
+	   }
+	 }
 	
    return 0;
  }
 
-void selectSort(string a[], int N)
+void selectSort(string a[], int N) 
  {
-   int k;
+   int k; 
    for(int i=0; i<N-1; ++i) 
     {
       k = i;
       for(int j=i+1; j<N; ++j) 
-       {
-         if (isLess(a[j],a[k])) k = j;
-       }
+        {
+          if (isLess(a[j],a[k])) k = j;
+        }
       swap(a[i],a[k]);
-    }    
+     }    
    return;
-}
+ }  
 
