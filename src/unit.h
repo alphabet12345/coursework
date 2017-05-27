cbe89bfb1d17014f618bfb7731cd150710b0562d
Svetlana <ssvetlana311223@gmail.com>
2017-05-28 01:57:16 +0700

    Structure

---
+++ src/unit.h
@@ -0,0 +1,11 @@
+#ifndef UNIT
+#define UNIT
+
+#include <string.h>
+#include <stdio.h>
+
+int selection(char text[]);
+int isLess(char a[], char b[]);
+void selectSort(char a[][20], int N);
+
+#endif
