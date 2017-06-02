828fc09446469b68c11090b9f1e68c28cd5e8fc2
ValeriaP <mardanoova@gmail.com>
2017-06-02 21:29:22 +0700

    Fixed bugs

--- src/unit.h
+++ src/unit.h
@@ -7,7 +7,7 @@
 int selection(char text[]);
 int isLess(char a[], char b[]);
 int isMore(char a[], char b[]);
-int selectSort(char a[][20], int N);
+int selectSort(char a[][100], int N);
 int swap(char a[], char b[]);
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,37 +1,37 @@
 CTEST (isLess, test0) {
-    char argument0[20] = "aa";
-    char argument1[20] = "bb";
+    char argument0[100] = "aa";
+    char argument1[100] = "bb";
     const int result = isLess(argument0,argument1);
     const int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST (isLess, test1) {
-    char argument0[20] = "bb";
-    char argument1[20] = "aa";
+    char argument0[100] = "bb";
+    char argument1[100] = "aa";
     const int result = isLess(argument0,argument1);
     const int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST (isMore, test0) {
-    char argument0[20] = "aa";
-    char argument1[20] = "bb";
+    char argument0[100] = "aa";
+    char argument1[100] = "bb";
     const int result = isMore(argument0,argument1);
     const int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST (isMore, test1) {
-    char argument0[20] = "bb";
-    char argument1[20] = "aa";
+    char argument0[100] = "bb";
+    char argument1[100] = "aa";
     const int result = isMore(argument0,argument1);
     const int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST (selectSort, test0) {
-    char argument0[4][20] = {"aa","bb","ab","dd"};
+    char argument0[4][100] = {"aa","bb","ab","dd"};
     const int argument1 = 4;
     const int result = selectSort(argument0,argument1);
     const int expected = 1;
